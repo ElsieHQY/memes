cb8c6fe43ae8b0b04986ea4347b040a9cc6f59df
Elsie Hsu <twilight10088@gmail.com>
2022-11-25 12:55:59 +0800

    delete unnecessary comments

--- src/memes.tsx
+++ src/memes.tsx
@@ -3,13 +3,6 @@ class Memes {
   public static Data: any;
   public static DataLength: number;
 
-  //   public static RandomNumber: number = Math.floor(
-  //     Math.random() * Memes.DataLength
-  //   );
-  // public static DisplayMeme: any[] = [
-  //   Memes.Data.data.memes[Memes.RandomNumber],
-  // ];
-
   public async fetchMemes() {
     try {
       const result = await fetch(`https://api.imgflip.com/get_memes`);
@@ -24,32 +17,6 @@ class Memes {
 
   constructor() {
     this.fetchMemes();
-    // Memes.RandomNumber = Math.floor(Math.random() * Memes.DataLength);
-    // Memes.DisplayMeme.push(Memes.Data.data.memes[Memes.RandomNumber]);
   }
 }
 export default Memes;
-
-// class FetchTranslation {
-//   public static instance: FetchTranslation = new FetchTranslation();
-//   public static fetchResult: FetchTranslation;
-//   public static fetchResultKeys: string[];
-//
-//   public async fetchTrans() {
-//     try {
-//       const res = await fetch("/translation.json");
-//       const data = await res.json();
-//       const keys = Object.keys(data);
-//       return (
-//         (FetchTranslation.fetchResult = data),
-//         (FetchTranslation.fetchResultKeys = keys)
-//       );
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   }
-//
-//   constructor() {
-//     this.fetchTrans();
-//   }
-// }
